d02bd43f1ca217438978247f6d0025836974198d
YoussefAbdelnaser <sbcaccount28@yahoo.com>
2023-07-10 16:42:53 +0300

    Started working on user routes and auth

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ require("express-async-errors");
 const express = require("express");
 const cors = require("cors");
 const connection = require("./db");
+const userRoutes = require("./routes/users.js");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 connection();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/login", authRoutes);
 
 const port = process.env.PORT;
 app.listen(port, console.log("server connected"));
